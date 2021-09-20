82a0c0f239246acd8ad434d6722f905298e469b6
dCOMMICS <sanwurld@gmail.com>
2021-09-20 17:51:39 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,26 @@
+package com.company;
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello Bro");
+        System.out.println("My name is Craig ");
+        //sUM
+        int fIRSTvALUE= 58;
+        int sECONDvALUE = 45;
+        System.out.println(fIRSTvALUE + sECONDvALUE);
+        System.out.println();
+        //VARIABLES
+        var fIRSTnUMBER  = 75;
+        var lASTnUMBER = 25;
+
+        var Ag = fIRSTnUMBER - lASTnUMBER;
+        System.out.println(Ag);
+        if (Ag< 15){
+            System.out.println("Wonderful");
+        }
+else {
+            System.out.println("haha");
+        }
+    }
+}
